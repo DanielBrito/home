9c151397f3e1975c27ae3eb0f5628857f42e660f
Daniel <daniell-henrique@hotmail.com>
2020-04-10 00:03:10 -0300

    Updates

---
+++ precache-manifest.3ccdc3539eebd107758d1ab9174337f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1efbb50aa58f0279ac28d184535ce942",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "f568651fabe7d2353e7b",
+    "url": "/home/static/css/main.888ee958.chunk.css"
+  },
+  {
+    "revision": "1f268dcdca07eae23dd1",
+    "url": "/home/static/js/2.efe97fba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/home/static/js/2.efe97fba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f568651fabe7d2353e7b",
+    "url": "/home/static/js/main.19fddecb.chunk.js"
+  },
+  {
+    "revision": "9a21ece79eae19b95cd8",
+    "url": "/home/static/js/runtime-main.68cf1828.js"
+  },
+  {
+    "revision": "0c84015b9e535f0b43d24b7527a70c3d",
+    "url": "/home/static/media/logo.0c84015b.png"
+  }
+]);
