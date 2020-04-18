d8f24a0e881fd9902a28654c6febf4971f964daa
Daniel <daniell-henrique@hotmail.com>
2020-04-18 20:24:52 -0300

    Routes configured

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './App.css';
+
 import Home from '../src/views/Home/Home';
+import About from '../src/views/About/About';
+import Books from '../src/views/Books/Books';
+import Projects from '../src/views/Projects/Projects';
+import Pictures from '../src/views/Pictures/Pictures';
+import Videos from '../src/views/Videos/Videos';
+import Blog from '../src/views/Blog/Blog';
+import Media from '../src/views/Media/Media';
+import Contact from '../src/views/Contact/Contact';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home/>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/home' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/books' component={Books} />
+            <Route path='/projects' component={Projects} />
+            <Route path='/pictures' component={Pictures} />
+            <Route path='/videos' component={Videos} />
+            <Route path='/blog' component={Blog} />
+            <Route path='/media' component={Media} />
+            <Route path='/contact' component={Contact} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
