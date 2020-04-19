755badfe7dd1be32dde27b02d11983b7bccf5798
Daniel <daniell-henrique@hotmail.com>
2020-04-18 23:00:30 -0300

    PICTURES layout

--- src/views/Pictures/Pictures.js
+++ src/views/Pictures/Pictures.js
@@ -1,10 +1,164 @@
 import React from "react";
 
-export default function Pictures() {
+// nodejs library that concatenates classes
+import classNames from "classnames";
+
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from '@material-ui/core/Grid';
+
+import Header from "../../components/Header/Header";
+import HeaderLinks from "../../components/Header/HeaderLinks";
+import Footer from "../../components/Footer/Footer";
+
+import GridContainer from "../../components/Grid/GridContainer";
+import GridItem from "../../components/Grid/GridItem";
+import Parallax from "../../components/Parallax/Parallax";
+
+import styles from "../../assets/jss/home/views/home.js";
+
+import srcLogo from "../../assets/img/logo.png"
+
+import img0 from "../../assets/img/img0.jpg"
+import img1 from "../../assets/img/img1.jpg"
+
+import Particles from 'react-particles-js'
+import Typewriter from 'typewriter-effect';
+
+const useStyles = makeStyles(styles);
+
+export default function Pictures(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+  const logo = <img src={srcLogo} alt="Daniel Brito logo" style={{ width: 55, height: 55 }} />
 
   return (
     <div>
-      PICTURES
+      <Header
+        color="transparent"
+        brand={logo}
+        rightLinks={<HeaderLinks />}
+        fixed
+        changeColorOnScroll={{
+          height: 100,
+          color: "neon"
+        }}
+        {...rest}
+      />
+
+      <Parallax image={require("../../assets/img/background.svg")}>
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem>
+              <div className={classes.brand}>
+                <h1 className={classes.title}>
+                  <Typewriter
+                    options={{
+                      autoStart: true,
+                      loop: false,
+                      cursor: "|",
+                      delay: 150
+                    }}
+                    onInit={(typewriter) => {
+                      typewriter
+                        .typeString('~$ PICTURES')
+                        .start();
+                    }
+                    }
+                  />
+                </h1>
+              </div>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </Parallax>
+
+      <Particles id="particles-js"
+        params={{
+          "particles": {
+            "number": {
+              "value": 200,
+              "density": {
+                "enable": false
+              }
+            },
+            "size": {
+              "value": 3,
+              "random": true,
+              "anim": {
+                "speed": 3,
+                "size_min": 0.3
+              }
+            },
+            "line_linked": {
+              "enable": false
+            },
+            "move": {
+              "random": true,
+              "speed": 0.1,
+              "direction": "top",
+              "out_mode": "out"
+            }
+          },
+          "interactivity": {
+            "events": {
+              "onhover": {
+                "enable": true,
+                "mode": "bubble"
+              },
+              "onclick": {
+                "enable": true,
+                "mode": "repulse"
+              }
+            },
+            "modes": {
+              "bubble": {
+                "distance": 250,
+                "duration": 2,
+                "size": 0,
+                "opacity": 0
+              },
+              "repulse": {
+                "distance": 400,
+                "duration": 4
+              }
+            }
+          }
+        }}
+        style={{
+          backgroundColor: "#000000",
+          opacity: 1,
+          float: "left",
+          left: 0,
+          top: 0,
+          position: "fixed",
+          width: "100%",
+          height: "100%",
+          zIndex: "-1"
+        }}
+      />
+
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <Grid container spacing={5} justify="center" alignItems="center">
+
+          <Grid item xs={12} sm={6} style={{ marginTop: "30px", marginBottom: "30px" }}>
+            <div>
+              <img src={img0} alt="Learn" width="100%" style={{ borderRadius: "10px", border: "1px solid #39ff14" }}></img>
+            </div>
+          </Grid>
+
+          <Grid item xs={12} sm={6} style={{ marginTop: "30px", marginBottom: "30px" }}>
+            <div>
+              <img src={img1} alt="Learn" width="100%" style={{ borderRadius: "10px", border: "1px solid #39ff14" }}></img>
+            </div>
+          </Grid>
+
+
+
+        </Grid>
+      </div>
+
+      <Footer />
     </div>
   );
 }
