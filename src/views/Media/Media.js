593c6324e7a1cf52a918395529e8c8e9b104bbc4
Daniel <daniell-henrique@hotmail.com>
2020-04-21 21:27:32 -0300

    MEDIA page finished

--- src/views/Media/Media.js
+++ src/views/Media/Media.js
@@ -19,8 +19,15 @@ import styles from "../../assets/jss/home/views/home.js";
 
 import srcLogo from "../../assets/img/logo.png"
 
+import imgPortalSertoes from "../../assets/img/halugar_portal_sertoes_240220.jpg"
+import imgUFC from "../../assets/img/halugar_ufc_crateus_noticias_010320.jpg"
+
 import Particles from 'react-particles-js'
 import Typewriter from 'typewriter-effect';
+import ReactPlayer from 'react-player';
+
+import Fade from 'react-reveal/Fade';
+import Zoom from 'react-reveal/Zoom';
 
 const useStyles = makeStyles(styles);
 
@@ -32,6 +39,10 @@ export default function Media(props) {
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
+
+  useEffect(() => {
+    document.title = "Daniel Brito - MEDIA"
+  })
 
   return (
     <div>
@@ -140,20 +151,86 @@ export default function Media(props) {
       />
 
       <div className={classNames(classes.main, classes.mainRaised)}>
-        <Grid container spacing={5} justify="center" alignItems="center">
-          <Grid item xs={12} sm={6} style={{marginTop: "30px", marginBottom: "30px"}}>
+        <Grid container spacing={5} justify="center" alignItems="center" style={{ marginBottom: "50px" }}>
+          <Grid item lg={6}>
             <div style={{ backgroundColor: "#000000", color: "#FFFFFF" }}>
-              VIDEO/NEWS
+              <Zoom>
+              <img src={imgUFC} alt="Learn" width="100%" style={{ borderRadius: "10px", border: "1px solid #39ff14" }}></img>
+              </Zoom>
             </div>
           </Grid>
-          <Grid item xs={12} sm={6}>
+          <Grid item lg={6}>
+            <Fade>
             <div style={{ backgroundColor: "#000000", color: "#FFFFFF" }}>
-              DESCRIPTION
+              <em><span style={{ fontWeight: "bold", fontSize: "20px", fontFamily: "monospace", color: "#39ff14" }}>"</span>The app <a href="https://play.google.com/store/apps/details?id=br.ufc.crateus.halugar" target="_blank" rel="noopener noreferrer" className={classes.contactLink} style={{ textDecoration: "none", fontWeight: "bold", color: "white" }} title="Download from Play Store">HáLugar</a> has gained the attention of city residents and, in a few days after its launch, it already had advertisements registered. For the professor of the discipline, who encouraged the publication of the software, the initiative will encourage the development of more ideas by students.<span style={{ fontWeight: "bold", fontSize: "20px", fontFamily: "monospace", color: "#39ff14" }}>"</span></em>
             </div>
+            </Fade>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={5} justify="center" direction="row-reverse" alignItems="center" style={{ marginTop: "50px", marginBottom: "50px" }}>
+          <Grid item lg={6}>
+            <div style={{ backgroundColor: "#000000", color: "#FFFFFF" }}>
+              <Zoom>
+              <img src={imgPortalSertoes} alt="Learn" width="100%" style={{ borderRadius: "10px", border: "1px solid #39ff14" }}></img>
+              </Zoom>
+            </div>
+          </Grid>
+          <Grid item lg={6}>
+            <Fade>
+            <div style={{ backgroundColor: "#000000", color: "#FFFFFF" }}>
+              <em><span style={{ fontWeight: "bold", fontSize: "20px", fontFamily: "monospace", color: "#39ff14" }}>"</span><a href="https://play.google.com/store/apps/details?id=br.ufc.crateus.halugar" target="_blank" rel="noopener noreferrer" className={classes.contactLink} style={{ textDecoration: "none", fontWeight: "bold", color: "white" }} title="Download from Play Store">HáLugar</a> app was developed by student Daniel Brito, from the Computer Science course at the Federal University of Ceará (Crateús Campus), during the disciplines of Web and Mobile Development, taught by professor Francisco Anderson de Almada Gomes.<span style={{ fontWeight: "bold", fontSize: "20px", fontFamily: "monospace", color: "#39ff14" }}>"</span></em>
+            </div>
+            </Fade>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={5} justify="center" alignItems="center" style={{ marginBottom: "30px" }}>
+          <Grid item lg={7}>
+            <div style={{ backgroundColor: "#000000", color: "#FFFFFF" }}>
+              <ReactPlayer width="100%" url='https://www.youtube.com/watch?v=LVshZDPiIy8' />
+            </div>
+          </Grid>
+          <Grid item lg={5}>
+            <Fade>
+            <div style={{ backgroundColor: "#000000", color: "#FFFFFF" }}>
+              Brief tribute by Paola Silva in her project called Curtas Poesias.
+            </div>
+            </Fade>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={5} justify="center" alignItems="center" direction="row-reverse" style={{ marginTop: "30px", marginBottom: "30px"}}>
+          <Grid item lg={7}>
+            <div style={{ backgroundColor: "#000000", color: "#FFFFFF" }}>
+              <ReactPlayer width="100%" url='https://www.youtube.com/watch?v=wSN3jBGo5pQ' />
+            </div>
+          </Grid>
+          <Grid item lg={5}>
+            <Fade>
+            <div style={{ backgroundColor: "#000000", color: "#FFFFFF" }}>
+              <em><span style={{ fontWeight: "bold", fontSize: "20px", fontFamily: "monospace", color: "#39ff14" }}>"</span>Art é Arte! shows the surprising harmony within this “anarchic” event and reveals the voice of artists from the periphery.<span style={{ fontWeight: "bold", fontSize: "20px", fontFamily: "monospace", color: "#39ff14" }}>"</span></em>
+            </div>
+            </Fade>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={5} justify="center" alignItems="center" style={{ marginTop: "30px" }}>
+          <Grid item lg={7}>
+            <div style={{ backgroundColor: "#000000", color: "#FFFFFF" }}>
+              <ReactPlayer width="100%" url='https://www.youtube.com/watch?v=T3VHeEjBwS0' />
+            </div>
+          </Grid>
+          <Grid item lg={5}>
+            <Fade>
+            <div style={{ backgroundColor: "#000000", color: "#FFFFFF" }}>
+              My first interview, which was granted to my colleagues from TV Grajaú.
+            </div>
+            </Fade>
           </Grid>
         </Grid>
       </div>
-      
+
       <Footer />
     </div>
   );
